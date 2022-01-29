8fba6c162f437eb895186a37ac6a39ffb0f5bfd3
Irina Tregub <irinatregub@gmail.com>
2022-01-29 21:39:44 +0200

    Completed recipe search and displaying name/picture

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, Image } from 'react-native';
 
 export default function App() {
+  const[keyword, setKeyword] = useState("");
+  const[recipes, setRecipes] = useState([]);
+
+  const getRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseJson => setRecipes(responseJson.meals))
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+          <View>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+            <Image style={styles.img} source={{uri: item.strMealThumb }}/>
+          </View>}
+        data={recipes} 
+        ItemSeparatorComponent={listSeparator} /> 
+
+      <TextInput style={{fontSize:18, width:200}} 
+      placeholder='Enter ingredient'
+      onChangeText={text => setKeyword(text) } />
+
+      <Button title="Find" onPress= {getRecipes} />
     </View>
   );
 }
@@ -16,5 +55,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 35,
   },
+
+  img : {
+    width: 50,
+    height: 50,
+  }
 });
